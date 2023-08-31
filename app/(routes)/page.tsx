0955fff9483b0066abf6a8c734dcdd38fb36e72b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-31 19:58:53 -0400

    feat: clockwise spinner

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,10 @@
 export default function HomePage() {
   return (
-    <>
-      <p>HomePage</p>
-    </>
+    <div className="flex justify-center items-center">
+      <div></div>
+      <div className="absolute border-2 border-red-500 h-20 w-20 spin-xclockwise" />
+      <div className="absolute border-2 border-emerald-500 h-20 w-20 spin-clockwise" />
+      <p className="absolute">JB</p>
+    </div>
   );
 }
