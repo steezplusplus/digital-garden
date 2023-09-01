467533a21a7a62958730e719ae1a206d066093e3
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-01 17:41:17 -0400

    feat: Scroll snap y

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -5,5 +5,20 @@ export const metadata: Metadata = {
 };
 
 export default function HomePage() {
-  return <div className="px-4 sm:px-6 lg:px-8">123</div>;
+  return (
+    <div className="h-full snap-y snap-mandatory overflow-scroll">
+      <div className="flex h-full w-full snap-start items-center justify-center bg-amber-200 text-8xl">
+        1
+      </div>
+      <div className="flex h-full w-full  snap-start items-center justify-center bg-teal-200 text-8xl">
+        2
+      </div>
+      <div className="flex h-full w-full snap-start items-center justify-center bg-cyan-200 text-8xl">
+        3
+      </div>
+      <div className="flex h-full w-full snap-start items-center justify-center bg-fuchsia-200 text-8xl">
+        4
+      </div>
+    </div>
+  );
 }
