3cef5ecb6c4cf2450681e2843e9acafd8a05ba96
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-01 18:56:07 -0400

    feat: Add headings

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,17 +7,17 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <div className="h-full snap-y snap-mandatory overflow-scroll">
-      <div className="flex h-full w-full snap-start items-center justify-center bg-amber-200 text-8xl">
-        1
+      <div className="flex h-full w-full snap-start items-center justify-center bg-amber-200">
+        <h2 className="text-2xl">Ecommerce Admin</h2>
       </div>
-      <div className="flex h-full w-full  snap-start items-center justify-center bg-teal-200 text-8xl">
-        2
+      <div className="flex h-full w-full  snap-start items-center justify-center bg-cyan-200">
+        <h2 className="text-2xl">Ecommerce Store</h2>
       </div>
-      <div className="flex h-full w-full snap-start items-center justify-center bg-cyan-200 text-8xl">
-        3
+      <div className="flex h-full w-full snap-start items-center justify-center bg-fuchsia-200">
+        <h2 className="text-2xl">Accessible Web Components</h2>
       </div>
-      <div className="flex h-full w-full snap-start items-center justify-center bg-fuchsia-200 text-8xl">
-        4
+      <div className="flex h-full w-full snap-start items-center justify-center bg-teal-200">
+        <h2 className="text-2xl">Chirp Chirp</h2>
       </div>
     </div>
   );
