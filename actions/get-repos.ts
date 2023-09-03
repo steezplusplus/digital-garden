66f7f7846b7adf54a51120b5ab78a413f73c72c8
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-03 17:11:00 -0400

    feat: Use official github typescript types

--- actions/get-repos.ts
+++ actions/get-repos.ts
@@ -1,5 +1,9 @@
+import { Endpoints } from '@octokit/types';
+
 const GITHUB_USERNAME = process.env['GITHUB_USERNAME'];
 const GITHUB_TOKEN = process.env['GITHUB_TOKEN'];
+
+type Repos = Endpoints['GET /users/{username}/repos']['response'];
 
 export async function getRepos() {
   const apiUrl = `https://api.github.com/users/${GITHUB_USERNAME}/repos`;
@@ -8,12 +12,12 @@ export async function getRepos() {
   const res = await fetch(apiUrl, headers);
 
   if (!res.ok) {
-    console.error('[GET_REPO]', res.status);
+    console.error('[GET_REPOS]', res.status);
   }
 
-  const json = await res.json();
+  const repos: Repos[] = await res.json();
 
-  const repos = json
+  const filteredRepos = repos
     .filter((repo: any) => !repo.private)
     .map((repo: any) => {
       return {
@@ -22,5 +26,5 @@ export async function getRepos() {
       };
     });
 
-  return repos;
+  return filteredRepos;
 }
