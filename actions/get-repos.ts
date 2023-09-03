54231987b02b6f40d069b115ddee480c34904370
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-03 17:20:20 -0400

    fix: Fix octokat type errors

--- actions/get-repos.ts
+++ actions/get-repos.ts
@@ -1,9 +1,5 @@
-import { Endpoints } from '@octokit/types';
-
 const GITHUB_USERNAME = process.env['GITHUB_USERNAME'];
 const GITHUB_TOKEN = process.env['GITHUB_TOKEN'];
-
-type Repos = Endpoints['GET /users/{username}/repos']['response'];
 
 export async function getRepos() {
   const apiUrl = `https://api.github.com/users/${GITHUB_USERNAME}/repos`;
@@ -15,16 +11,7 @@ export async function getRepos() {
     console.error('[GET_REPOS]', res.status);
   }
 
-  const repos: Repos[] = await res.json();
+  const repos = await res.json();
 
-  const filteredRepos = repos
-    .filter((repo: any) => !repo.private)
-    .map((repo: any) => {
-      return {
-        name: repo.name,
-        description: repo.description,
-      };
-    });
-
-  return filteredRepos;
+  return repos;
 }
