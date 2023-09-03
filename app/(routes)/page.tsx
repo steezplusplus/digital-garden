b7083e02b8e4addaf09a9dd1b479648483b7f246
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-03 19:30:15 -0400

    feat: Create SnapSection

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 import { getRepos, getUser } from '@/octokit';
+import { SnapContainer, SnapSection } from './_components/scroll-snap';
 
 export const metadata: Metadata = {
   title: "Jesse's Homepage",
@@ -9,21 +10,22 @@ export const metadata: Metadata = {
 export default async function HomePage() {
   const user = await getUser();
   const repos = await getRepos();
+  console.log(user, repos);
 
   return (
-    <div className="h-full snap-y snap-mandatory overflow-scroll">
-      <div className="flex h-full w-full snap-start items-center justify-center bg-amber-200">
+    <SnapContainer>
+      <SnapSection>
         <h2 className="text-2xl">Ecommerce Admin</h2>
-      </div>
-      <div className="flex h-full w-full  snap-start items-center justify-center bg-cyan-200">
+      </SnapSection>
+      <SnapSection>
         <h2 className="text-2xl">Ecommerce Store</h2>
-      </div>
-      <div className="flex h-full w-full snap-start items-center justify-center bg-fuchsia-200">
+      </SnapSection>
+      <SnapSection>
         <h2 className="text-2xl">Accessible Web Components</h2>
-      </div>
-      <div className="flex h-full w-full snap-start items-center justify-center bg-teal-200">
+      </SnapSection>
+      <SnapSection>
         <h2 className="text-2xl">Chirp Chirp</h2>
-      </div>
-    </div>
+      </SnapSection>
+    </SnapContainer>
   );
 }
