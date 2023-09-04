172cc4f119f9dec364cebdddaaf232b4ba32eeff
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-04 18:33:40 -0400

    feat: Export octokit client

--- octokit.ts
+++ octokit.ts
@@ -2,7 +2,7 @@ import { Octokit } from '@octokit/rest';
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-const octokit = new Octokit({
+export const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 });
 
