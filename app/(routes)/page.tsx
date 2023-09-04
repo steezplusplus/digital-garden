f9f253927f93018bf80db14217cc7f8502232a53
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-03 20:58:20 -0400

    feat: Create project page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,30 +1,10 @@
 import { Metadata } from 'next';
 
-import { getRepos } from '@/octokit';
-import { SnapContainer, SnapSection } from './_components/scroll-snap';
-
 export const metadata: Metadata = {
   title: "Jesse's Homepage",
 };
 
 export default async function HomePage() {
   // const user = await getUser(); // TODO
-  const repos = await getRepos();
-
-  return (
-    <SnapContainer>
-      {repos.map((repo) => (
-        <SnapSection key={repo.name}>
-          <div className="flex flex-col">
-            <h2 className="text-2xl">{repo.name}</h2>
-            <p>{repo.description}</p>
-            <p>Last updated {repo.updated_at}</p>
-            <a href={repo.url}>Github link</a>
-            {repo.homepage && <a href={repo.homepage}>Live link</a>}
-            <p>{repo.num_stars} stars</p>
-          </div>
-        </SnapSection>
-      ))}
-    </SnapContainer>
-  );
+  return <div>homepage</div>;
 }
