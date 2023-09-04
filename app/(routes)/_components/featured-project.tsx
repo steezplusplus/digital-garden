1df37e31af1b6fb38f98cc1f27e6d44c2af235f3
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-04 19:15:24 -0400

    refactor: Reorganize imports and export order

--- app/(routes)/_components/featured-project.tsx
+++ app/(routes)/_components/featured-project.tsx
@@ -22,6 +22,32 @@ export const featProjects = [
   },
 ];
 
+export type FeaturedProject = {
+  displayName: string;
+  repoName: string;
+};
+
+export async function FeaturedProject(props: FeaturedProject) {
+  const { displayName, repoName } = props;
+  const project = await getRepo(repoName);
+
+  return (
+    <Link
+      href={project.html_url}
+      target="_blank"
+      className="aspect-auto rounded-xl border border-stone-400 p-4"
+    >
+      <h2>{displayName}</h2>
+      <h3>{project.description}</h3>
+      <p>{project.num_stars} stars</p>
+      <p>{project.num_forks} forks</p>
+      <p>{project.num_subscribers} subscribers</p>
+      <p>Built with {project.language}</p>
+      {project.license && <p>{project.license.name}</p>}
+    </Link>
+  );
+}
+
 async function getRepo(repo: string) {
   if (!GITHUB_USERNAME) {
     throw new Error('[GET_REPO]: GITHUB_USERNAME is required');
@@ -48,30 +74,4 @@ async function getRepo(repo: string) {
     console.error('[GET_REPO]');
     throw error;
   }
-}
-
-export type FeaturedProject = {
-  displayName: string;
-  repoName: string;
-};
-
-export async function FeaturedProject(props: FeaturedProject) {
-  const { displayName, repoName } = props;
-  const project = await getRepo(repoName);
-
-  return (
-    <Link
-      href={project.html_url}
-      target="_blank"
-      className="aspect-auto rounded-xl border border-stone-400 p-4"
-    >
-      <h2>{displayName}</h2>
-      <h3>{project.description}</h3>
-      <p>{project.num_stars} stars</p>
-      <p>{project.num_forks} forks</p>
-      <p>{project.num_subscribers} subscribers</p>
-      <p>Built with {project.language}</p>
-      {project.license && <p>{project.license.name}</p>}
-    </Link>
-  );
 }
