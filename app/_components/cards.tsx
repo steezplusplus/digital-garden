5ee389968a5587bd6ab0acb6677a7c3fbfbb5bfa
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-04 15:16:04 -0400

    feat: Finish some cards

--- app/_components/cards.tsx
+++ app/_components/cards.tsx
@@ -1,27 +1,51 @@
+import Link from 'next/link';
+import { Linkedin, Twitter, Github } from 'lucide-react';
+
 export function GithubCard() {
   const label = 'Github Link';
+  const href = 'https://github.com/steezplusplus';
   return (
-    <div className="aspect-square rounded-xl border border-stone-400 p-4">
-      {label}
-    </div>
+    <Link
+      href={href}
+      target="_blank"
+      className="flex aspect-square flex-col rounded-xl border border-stone-400 p-4"
+    >
+      <h2 className="mb-1 text-lg first-letter:text-2xl">{label}</h2>
+      <h3 className="font-italic text-sm">SteezPlusPlus</h3>
+      <Github className="mt-auto self-end" />
+    </Link>
   );
 }
 
 export function TwitterCard() {
   const label = 'Twitter Link';
+  const href = 'https://twitter.com/CodingSteez';
   return (
-    <div className="aspect-square rounded-xl border border-stone-400 p-4">
-      {label}
-    </div>
+    <Link
+      href={href}
+      target="_blank"
+      className="flex aspect-square flex-col rounded-xl border border-stone-400 p-4"
+    >
+      <h2 className="mb-1 text-lg first-letter:text-2xl">{label}</h2>
+      <h3 className="text-sm">@CodingSteez</h3>
+      <Twitter className="mt-auto self-start" />
+    </Link>
   );
 }
 
 export function LinkedInCard() {
   const label = 'LinkedIn Link';
+  const href = 'https://www.linkedin.com/in/jesse-bp/';
   return (
-    <div className="aspect-square rounded-xl border border-stone-400 p-4">
-      {label}
-    </div>
+    <Link
+      href={href}
+      target="_blank"
+      className="flex aspect-square flex-col rounded-xl border border-stone-400 p-4"
+    >
+      <Linkedin className="mb-auto self-end" />
+      <h2 className="mb-1 text-lg first-letter:text-2xl">{label}</h2>
+      <h3 className="text-sm">in/jesse-bp</h3>
+    </Link>
   );
 }
 
