a33ff78158487b4a2062098055a09738bae13390
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-04 22:27:47 -0400

    feat: Create not featured schema

--- app/(routes)/_components/not-featured/not-featured-feed.tsx
+++ app/(routes)/_components/not-featured/not-featured-feed.tsx
@@ -1,14 +1,37 @@
-import { NotFeatured } from './not-featured';
+import { Suspense } from 'react';
 
+import { NotFeatured, NotFeaturedProps } from './not-featured';
+
+type NotFeaturedFeedProps = NotFeaturedProps & { key: string };
+
+// TODO Update project list
+const notFeaturedProjects: NotFeaturedFeedProps[] = [
+  {
+    displayName: 'Acessible Web Components',
+    repoName: 'Accessible-Web-Components',
+    key: 'Accessible-Web-Components-0',
+  },
+  {
+    displayName: 'History Plotter',
+    repoName: 'history-plotter',
+    key: 'history-plotter-1',
+  },
+  {
+    displayName: 'Istanbul Stock Exchange Analysis',
+    repoName: 'Istanbul-Stock-Exchange-Analysis',
+    key: 'Istanbul-Stock-Exchange-Analysis-2',
+  },
+];
+
+// TODO Create better suspsene fallback that creates less CLS
 export function NotFeaturedFeed() {
   return (
     <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      <NotFeatured />
-      <NotFeatured />
-      <NotFeatured />
-      <NotFeatured />
-      <NotFeatured />
-      <NotFeatured />
+      {notFeaturedProjects.map((project) => (
+        <Suspense key={project.key} fallback={<p>Loading...</p>}>
+          <NotFeatured {...project} />
+        </Suspense>
+      ))}
     </div>
   );
 }
--- app/(routes)/_components/not-featured/not-featured.tsx
+++ app/(routes)/_components/not-featured/not-featured.tsx
@@ -1,8 +1,36 @@
-export function NotFeatured() {
-  const label = 'Repo';
+import Link from 'next/link';
+import { getProject } from '@/octokit';
+
+export type NotFeaturedProps = {
+  displayName: string;
+  repoName: string;
+};
+
+// TODO Component UI
+export async function NotFeatured(props: NotFeaturedProps) {
+  const { displayName, repoName } = props;
+  const project = await getProject(repoName);
+
+  // TODO Error fallback UI?
+  if (project === undefined) {
+    return (
+      <div className="aspect-auto rounded-xl border border-stone-400 p-4">
+        <p>Uh-oh! Something went wrong.</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="aspect-auto rounded-xl border border-stone-400 p-4">
-      {label}
-    </div>
+    <Link
+      href={project.html_url}
+      target="_blank"
+      className="aspect-auto rounded-xl border border-stone-400 p-4"
+    >
+      <h2>{displayName}</h2>
+      <h3>{project.description}</h3>
+      <p>{project.num_stars} stars</p>
+      <p>{project.num_forks} forks</p>
+      <p>{project.num_subscribers} subscribers</p>
+    </Link>
   );
 }
