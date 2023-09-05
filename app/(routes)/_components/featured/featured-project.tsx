99eb57afd8a96ac6417d68b8fd0341a7149d4429
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-05 00:38:22 -0400

    feat: Iconography

--- app/(routes)/_components/featured/featured-project.tsx
+++ app/(routes)/_components/featured/featured-project.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image';
+import { GitFork, Star } from 'lucide-react';
 
 import { getProject } from '@/octokit';
 
@@ -32,42 +32,14 @@ export async function FeaturedProject(props: FeaturedProjectProps) {
       <p className="p-2">{project.description}</p>
       <div className="mt-auto">
         <div className="flex items-center gap-x-2">
-          <p className="rounded border px-2 py-1 text-xs">
-            {project.num_stars} stars
+          <p className="flex items-center gap-x-2 rounded border px-2 py-1 text-xs">
+            {project.num_stars} <Star size={16} />
           </p>
-          <p className="rounded border px-2 py-1 text-xs">
-            {project.num_forks} forks
+          <p className="flex items-center gap-x-2 rounded border px-2 py-1 text-xs">
+            {project.num_forks} <GitFork size={16} />
           </p>
         </div>
       </div>
     </Link>
   );
-
-  // return (
-  //   <Link
-  //     href={project.html_url}
-  //     target="_blank"
-  //     className="aspect-auto rounded-xl border border-stone-400 p-4"
-  //   >
-  //     <h2>{displayName}</h2>
-  //     <h3>{project.description}</h3>
-  //     <p>{project.num_stars} stars</p>
-  //     <p>{project.num_forks} forks</p>
-  //     <p>{project.num_subscribers} subscribers</p>
-  //     <p>Built with {project.language}</p>
-  //     {project.license && <p>{project.license.name}</p>}
-  //   </Link>
-  // );
-}
-
-{
-  /* 
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-  <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
-  <div class="flex flex-col justify-between p-4 leading-normal">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-  </div>
-</a> 
-*/
 }
