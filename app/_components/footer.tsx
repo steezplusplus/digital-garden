41b49fa2089e5d78aca8e8846dd8804d67b211a0
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-06 14:47:01 -0400

    feat: Add links to footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,10 +1,40 @@
+import { Github } from 'lucide-react';
+import Link from 'next/link';
+
+const githubProfileUrl = 'https://github.com/steezplusplus';
+const githubRepoUrl = 'https://github.com/steezplusplus/Portfolio';
+
 export function Footer() {
   return (
-    <ul className="flex h-full items-center px-4 sm:px-6 lg:px-8">
+    <ul className="flex h-full items-center px-4 text-sm sm:px-6 lg:px-8">
       <li>
-        <p className="text-sm">
-          &copy; Jesse Breuer-Penello. All rights reserverd.
-        </p>
+        <ul className="flex flex-col gap-2">
+          <li>
+            <p>
+              <span className="mr-1">&copy;</span>Jesse Breuer-Penello. All
+              rights reserverd.
+            </p>
+          </li>
+          <li>
+            <Link
+              className="flex items-center text-blue-500 underline hover:text-blue-300"
+              href={githubProfileUrl}
+              target="_blank"
+            >
+              <Github size={16} className="mr-1" />
+              SteezPlusPlus
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className="ml-auto">
+        <Link
+          className="text-blue-500 underline hover:text-blue-300"
+          href={githubRepoUrl}
+          target="_blank"
+        >
+          View source code
+        </Link>
       </li>
     </ul>
   );
