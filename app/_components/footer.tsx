7b5580b7b9d8275b90aeaca719cb6b2757bb4f3f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-06 18:56:53 -0400

    style: Fix mobile and desktop layout

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -4,14 +4,11 @@ const githubRepoUrl = 'https://github.com/steezplusplus/Portfolio';
 
 export function Footer() {
   return (
-    <ul className="grid h-full grid-cols-1 px-4 text-xs sm:grid-cols-2 sm:px-6 lg:px-8">
-      <li className="flex items-center">
-        <p>
-          <span className="mr-1">&copy;</span>Jesse Breuer-Penello. All rights
-          reserverd.
-        </p>
+    <ul className="flex h-full flex-col items-center justify-center px-4 text-sm sm:flex-row sm:justify-between sm:px-6 lg:px-8">
+      <li>
+        <p>&copy; Jesse Breuer-Penello. All rights reserverd.</p>
       </li>
-      <li className="flex items-center">
+      <li>
         <Link
           className="text-blue-500 underline hover:text-blue-300"
           href={githubRepoUrl}
