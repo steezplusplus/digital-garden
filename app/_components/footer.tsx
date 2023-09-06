964f52ab2832b35fcdea8c44a162cd10d5d1befc
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-06 15:08:01 -0400

    style: Simplify footer styles

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,33 +1,17 @@
-import { Github } from 'lucide-react';
 import Link from 'next/link';
 
-const githubProfileUrl = 'https://github.com/steezplusplus';
 const githubRepoUrl = 'https://github.com/steezplusplus/Portfolio';
 
 export function Footer() {
   return (
-    <ul className="flex h-full items-center px-4 text-sm sm:px-6 lg:px-8">
-      <li>
-        <ul className="flex flex-col gap-2">
-          <li>
-            <p>
-              <span className="mr-1">&copy;</span>Jesse Breuer-Penello. All
-              rights reserverd.
-            </p>
-          </li>
-          <li>
-            <Link
-              className="flex items-center text-blue-500 underline hover:text-blue-300"
-              href={githubProfileUrl}
-              target="_blank"
-            >
-              <Github size={16} className="mr-1" />
-              SteezPlusPlus
-            </Link>
-          </li>
-        </ul>
+    <ul className="grid h-full grid-cols-1 px-4 text-xs sm:grid-cols-2 sm:px-6 lg:px-8">
+      <li className="flex items-center">
+        <p>
+          <span className="mr-1">&copy;</span>Jesse Breuer-Penello. All rights
+          reserverd.
+        </p>
       </li>
-      <li className="ml-auto">
+      <li className="flex items-center">
         <Link
           className="text-blue-500 underline hover:text-blue-300"
           href={githubRepoUrl}
