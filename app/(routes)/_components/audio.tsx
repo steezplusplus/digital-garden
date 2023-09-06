cb7872a299b8765babdaafc304c35bd4d11dd58f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-05 22:35:57 -0400

    feat: Add play pause

--- app/(routes)/_components/audio.tsx
+++ app/(routes)/_components/audio.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Timer, Speaker, Play } from 'lucide-react';
+import { useRef, useState, useEffect } from 'react';
+import { Timer, Speaker, Play, Pause } from 'lucide-react';
 
 type AudioProps = {
   songPath: string;
@@ -8,15 +9,38 @@ type AudioProps = {
 
 export function Audio(props: AudioProps) {
   const { songPath } = props;
+  const audioRef: React.RefObject<HTMLAudioElement> = useRef(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(60);
+
+  const togglePlayPause = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (audioRef && audioRef.current) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume, audioRef]);
+
+  useEffect(() => {
+    if (audioRef && audioRef.current && isPlaying) {
+      audioRef.current.play();
+    }
+    if (audioRef && audioRef.current && !isPlaying) {
+      audioRef.current.pause();
+    }
+  }, [isPlaying, audioRef]);
+
   return (
     <>
-      <audio>
-        <source src={songPath} />
+      <audio ref={audioRef} src={songPath}>
         Your browser does not support the <code>audio</code> element.
       </audio>
       <div className="my-auto flex h-1/3 items-center gap-x-4 rounded border border-slate-400">
-        <button className="h-full border-r px-2">
-          <Play size={16} />
+        <button className="h-full border-r px-2" onClick={togglePlayPause}>
+          {isPlaying ? <Pause size={16} /> : <Play size={16} />}
         </button>
         <div className="flex w-full flex-col gap-y-2 pr-4">
           <div className="flex w-full items-center gap-x-4">
@@ -25,7 +49,14 @@ export function Audio(props: AudioProps) {
           </div>
           <div className="flex w-full items-center gap-x-4">
             <Speaker size={16} />
-            <input className="w-full" type="range" max="100" min="0" />
+            <input
+              className="w-full"
+              type="range"
+              max="100"
+              min="0"
+              value={volume}
+              onChange={(e) => setVolume(Number(e.target.value))}
+            />
           </div>
         </div>
       </div>
