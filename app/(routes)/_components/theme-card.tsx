25f1b898808cf701349663ada8e4d588997770d0
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-06 17:25:10 -0400

    feat: Toggle icon alongside theme

--- app/(routes)/_components/theme-card.tsx
+++ app/(routes)/_components/theme-card.tsx
@@ -1,4 +1,3 @@
-import { Lightbulb } from 'lucide-react';
 import { ThemeSwitcher } from './theme-switcher';
 
 export function ThemeCard() {
@@ -6,10 +5,7 @@ export function ThemeCard() {
   return (
     <div className="col-span-2 flex aspect-auto flex-col rounded-xl border border-stone-400 p-4">
       <h2 className="mb-1 text-lg first-letter:text-2xl">{label}</h2>
-      <div className="my-auto flex justify-center">
-        <ThemeSwitcher />
-      </div>
-      <Lightbulb className="mt-auto self-end" />
+      <ThemeSwitcher />
     </div>
   );
 }
--- app/(routes)/_components/theme-switcher.tsx
+++ app/(routes)/_components/theme-switcher.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
+import { Lightbulb, Moon, SunMedium } from 'lucide-react';
 
 export function ThemeSwitcher() {
   const [mounted, setMounted] = useState<boolean>(false);
@@ -23,12 +24,19 @@ export function ThemeSwitcher() {
   };
 
   return (
-    <button
-      aria-pressed={isDark}
-      onClick={toggleDarkMode}
-      className="rounded border border-stone-400 px-3 py-2 text-sm"
-    >
-      Toggle
-    </button>
+    <>
+      <div className="my-auto flex justify-center">
+        <button
+          aria-pressed={isDark}
+          onClick={toggleDarkMode}
+          className="rounded border border-stone-400 px-3 py-2 text-sm"
+        >
+          Toggle
+        </button>
+      </div>
+      <div className="mt-auto self-end">
+        {isDark ? <Moon /> : <SunMedium />}
+      </div>
+    </>
   );
 }
