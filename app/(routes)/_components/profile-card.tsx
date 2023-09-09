6662289be38032796f378dde426bbbd18e9bfece
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-09 13:37:05 -0400

    feat: Primary asset

--- app/(routes)/_components/profile-card.tsx
+++ app/(routes)/_components/profile-card.tsx
@@ -1,15 +1,25 @@
+import Image from 'next/image';
+
 export function ProfileCard() {
   return (
     <div
       className="
         bottom-0 aspect-auto self-start
-        rounded-xl border border-stone-400  p-4 
+        rounded-xl border border-stone-400 p-4 
         sm:sticky sm:top-0 sm:aspect-square
       "
     >
       <div className="flex h-full flex-col items-center justify-center">
-        <h2 className="text-xl">Asset</h2>
-        <h2 className="text-xl">Welcome to my digital garden</h2>
+        <Image
+          src="/primary.jpg"
+          alt="A profile picture"
+          width="64"
+          height="64"
+          className="h-16 w-16 rounded-full"
+        />
+        <figcaption className="mt-2 text-center text-sm">
+          Welcome to my digital garden
+        </figcaption>
       </div>
     </div>
   );
