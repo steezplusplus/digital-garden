ecb9b5f0cd0da2202f493cdcc7fc4d9cb71d63a7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-12 17:26:09 -0400

    refactor: Clean up long classname

--- app/_components/footer/footer.tsx
+++ app/_components/footer/footer.tsx
@@ -4,7 +4,12 @@ const githubRepoUrl = 'https://github.com/steezplusplus/Portfolio';
 
 export function Footer() {
   return (
-    <ul className="flex h-full flex-col items-center justify-center px-4 text-sm sm:flex-row sm:justify-between sm:px-6 lg:px-8">
+    <ul
+      className="
+        flex h-full flex-col items-center justify-center px-4 text-sm
+        sm:flex-row sm:justify-between sm:px-6 lg:px-8
+      "
+    >
       <li>
         <p>&copy; Jesse Breuer-Penello. All rights reserverd.</p>
       </li>
