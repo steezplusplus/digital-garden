9849fdf590fd8058b0f82b3d255f82c9fabc501b
Jesse Breuer <penelloj2@gmail.com>
2023-09-14 14:34:13 -0400

    test: increase code coverage (#8)
    * test: Test Badge component
    
    * docs: Add codecov badge

---
+++ app/_components/cards/repo-card/badge/badge.tsx
@@ -0,0 +1,15 @@
+export function Badge() {
+  return (
+    <div
+      className="
+        absolute -right-2 -top-3
+        rounded-lg border border-stone-400
+      bg-indigo-50 px-2 py-1 text-xs
+        font-semibold text-black
+        dark:bg-neutral-900 dark:text-indigo-50
+      "
+    >
+      New!
+    </div>
+  );
+}
