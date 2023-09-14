93cde1008fecb3a288bc7b9e750bf6d851bcc710
Jesse Breuer <penelloj2@gmail.com>
2023-09-14 16:06:08 -0400

    test: Enhance unit tests (#10)

--- app/_components/cards/linkedin-card/linkedin-card.test.tsx
+++ app/_components/cards/linkedin-card/linkedin-card.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { LinkedinCard } from './linkedin-card';
 
 const props = {
@@ -11,5 +11,11 @@ const props = {
 describe('<LinkedinCard />', () => {
   it('Should render with no errors', () => {
     render(<LinkedinCard {...props} />);
+    const anchor = screen.getByRole('link');
+    const heading2 = screen.getByRole('heading', { level: 2 });
+    const heading3 = screen.getByRole('heading', { level: 3 });
+    expect(anchor).toHaveAttribute('href', props.href);
+    expect(heading2).toHaveTextContent(props.label);
+    expect(heading3).toHaveTextContent(props.subLabel);
   });
 });
