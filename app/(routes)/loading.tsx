398f753b56e14aa133c12f82a06ed853213eaa4d
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-13 21:09:47 -0400

    feat: Updatie loading ui

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -1,17 +1,9 @@
 import { GithubIcon, Twitter, Linkedin, Mail, Music } from 'lucide-react';
-import { Audio } from './_components/audio';
-import { ThemeSwitcher } from './_components/theme-switcher';
+
+import * as schema from '../_schema/schema';
 import { ProfileCard } from './_components/profile-card';
-import {
-  TopLeftRectangle,
-  TopRightRectangle,
-} from './_components/rectangle-cards';
-import {
-  TopLeftSquare,
-  TopRightSquare,
-  BottomLeftSquare,
-  BottomRightSquare,
-} from './_components/square-cards';
+import { TopLeftRectangle, TopRightRectangle } from './_components/rectangle-cards';
+import { TLSquare, TRSquare, BLSquare, BRSquare } from './_components/square-cards';
 
 // TODO Less CLS issues
 export default function Loading() {
@@ -20,46 +12,22 @@ export default function Loading() {
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         <ProfileCard />
         <div className="grid grid-cols-2 gap-4">
-          <TopLeftSquare
-            href="https://github.com/steezplusplus"
-            label="Github"
-            subLabel="SteezPlusPlus"
-            icon={<GithubIcon />}
-          />
-          <TopRightSquare
-            href="https://twitter.com/CodingSteez"
-            label="Twitter"
-            subLabel="@CodingSteez"
-            icon={<Twitter />}
-          />
-          <BottomLeftSquare
-            href="https://www.linkedin.com/in/jesse-bp/"
-            label="LinkedIn"
-            subLabel="in/jesse-bp"
-            icon={<Linkedin />}
-          />
-          <BottomRightSquare
-            href="mailto:PenelloJ2@gmail.com"
-            label="Contact"
-            subLabel="Email me"
-            icon={<Mail />}
-          />
-          <TopLeftRectangle label="Theme" icon={<></>}>
-            <ThemeSwitcher />
+          <TLSquare {...schema.githubSchema} />
+          <TRSquare {...schema.twitterSchema} />
+          <BLSquare {...schema.linkedinSchema} />
+          <BRSquare {...schema.contactSchema} />
+          <TopLeftRectangle {...schema.musicSchema}>
+            <></>
           </TopLeftRectangle>
-          <TopRightRectangle
-            label="Listening to..."
-            subLabel="LoFi"
-            icon={<Music />}
-          >
-            <Audio songPath="/song.mp3" />
+          <TopRightRectangle {...schema.themeSchema}>
+            <></>
           </TopRightRectangle>
         </div>
       </div>
 
       <div>
         <h2 className="mb-4 text-3xl">Featured Projects</h2>
-        <p>Loading...</p>
+        <></>
       </div>
     </>
   );
