43ffadc57ca4e1f0672ef3aecda50cc67081a948
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-14 22:12:25 -0400

    test: mock local storage

--- app/_components/providers/providers.test.tsx
+++ app/_components/providers/providers.test.tsx
@@ -2,10 +2,23 @@ import { render } from '@testing-library/react';
 import { Providers } from './providers';
 
 describe('<Providers />', () => {
-  const para = <p>asd</p>;
-
-  // TODO Replace next-theme
+  beforeEach(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+  const para = <p>anything</p>;
   it('Should render Providers with no errors', () => {
-    // render(<Providers>{para}</Providers>);
+    render(<Providers>{para}</Providers>);
   });
 });
