e312732f4f5b21f6d7b904b376cbd9808fff4b91
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-18 12:21:23 -0400

    feat: add support card

---
+++ app/_components/cards/support-card/support-card.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+type SupportCardProps = {
+  label: string;
+  subLabel?: string;
+  href: string;
+  image: JSX.Element;
+};
+
+export function SupportCard(props: SupportCardProps) {
+  const { label, subLabel, href, image } = props;
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      className="
+        col-span-2 flex aspect-auto flex-col rounded-xl
+        border border-stone-400 bg-white/50
+        p-4 dark:bg-indigo-100/5
+      "
+    >
+      <h2 className="mb-1 text-lg">{label}</h2>
+      <div className="mt-2 self-center">{image}</div>
+      {subLabel && <h3 className="self-end text-sm italic">{subLabel}</h3>}
+    </Link>
+  );
+}
