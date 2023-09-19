87ddb9df999e3cdf5eaea051e20d7ef02d603da5
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-09-19 11:34:35 -0400

    test: Increase coverage for contact card

--- app/_components/cards/contact-card/contact-card.test.tsx
+++ app/_components/cards/contact-card/contact-card.test.tsx
@@ -5,7 +5,7 @@ const props = {
   href: 'mailto:not-real@example.com',
   label: 'label',
   subLabel: 'subLabel',
-  icon: <div></div>,
+  icon: <svg data-testid="icon" />,
 };
 
 describe('<ContactCard />', () => {
@@ -14,8 +14,10 @@ describe('<ContactCard />', () => {
     const anchor = screen.getByRole('link');
     const heading2 = screen.getByRole('heading', { level: 2 });
     const heading3 = screen.getByRole('heading', { level: 3 });
+    const icon = screen.getByTestId('icon');
     expect(anchor).toHaveAttribute('href', props.href);
     expect(heading2).toHaveTextContent(props.label);
     expect(heading3).toHaveTextContent(props.subLabel);
+    expect(icon).toBeInTheDocument();
   });
 });
