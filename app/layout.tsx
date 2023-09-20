874f23896e8d4ec184c04ff71178f1ec3ac6e1bd
Jesse Breuer <penelloj2@gmail.com>
2023-09-20 18:41:45 -0400

    build: Install @vercel/analytics (#20)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Lora } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
 import { Nav } from './_components/nav/nav';
@@ -38,6 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={lora.className}>
+        <Analytics />
         <Providers>
           <nav>
             <Nav />
