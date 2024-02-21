0a9d781c5122e76b346b910bb37935b818a3795b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2024-02-21 16:48:55 -0500

    build: Apply tw to components dr

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   plugins: [],
 };
 export default config;
