f4a420cbe584b344c5b6a6f4d5c94f9d27efe4bd
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2024-06-27 18:01:01 -0400

    feat: Rotate profile card img on scroll

--- components/cards/profile-card/profile-card.tsx
+++ components/cards/profile-card/profile-card.tsx
@@ -1,36 +1,23 @@
 'use client';
 
 import Image from 'next/image';
-import { useRef, useEffect } from 'react';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
 export function ProfileCard() {
-  const spinnerRef = useRef<HTMLImageElement | null>(null);
-
-  useEffect(() => {
-    const element = spinnerRef.current;
-
-    if (element) {
-      gsap.to(element, {
-        rotation: 720,
-        ease: 'none',
-        scrollTrigger: {
-          trigger: element,
-          start: 'top center',
-          end: 'bottom center',
-          scrub: true,
-          markers: true, // This will add visual markers for debugging
-        },
-      });
-
-      // Clean up the ScrollTrigger instance on unmount
-      return () => {
-        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-      };
-    }
+  useGSAP(() => {
+    gsap.to('.rotate-img', {
+      scrollTrigger: {
+        trigger: '.rotate-container', // start and end animation when element with this classname enters and leaves viewport
+        start: '-40px top', // start at -40px above the rotate-container top of viewport
+        end: 'bottom top', // end at bottom of rotate-container reaches top of scroller end
+        scrub: true,
+      },
+      rotation: 360,
+    });
   });
 
   return (
@@ -42,8 +29,7 @@ export function ProfileCard() {
           alt="A profile picture"
           width="128"
           height="128"
-          className="aspect-auto h-32 w-32 rounded-full"
-          ref={spinnerRef}
+          className="rotate-img aspect-auto h-32 w-32 rounded-full"
         />
         <figcaption className="mt-2 text-center text-sm">Welcome to the garden.</figcaption>
       </div>
