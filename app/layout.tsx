ab275fd7e7f106b1cc24ad5113dce88d7695f7bc
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2025-01-01 14:09:04 -0500

    fix(ui): suppress warning from next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={lora.className}>
         <Analytics />
         <Providers>
