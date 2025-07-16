5dc6e85e84dc725daa1e7911484fafc31b879d67
Jesse Breuer <jessebreuer@Mac.lan>
2025-07-16 12:22:01 -0400

    build: update deps that are minor or patch behind

--- jest.config.ts
+++ jest.config.ts
@@ -12,10 +12,6 @@ const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   preset: 'ts-jest',
-  // Optional: Add module name mapping for cleaner GSAP imports
-  moduleNameMapping: {
-    '^gsap/dist/(.*)$': '<rootDir>/node_modules/gsap/dist/$1',
-  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
