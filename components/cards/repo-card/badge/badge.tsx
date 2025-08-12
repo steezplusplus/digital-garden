f98ae6ac210f1d9fc5da6cc0676f9cca966f5024
Jesse Breuer <jessebreuer@Mac.lan>
2025-08-12 14:58:15 -0400

    build: update and run prettier and eslint

--- components/cards/repo-card/badge/badge.tsx
+++ components/cards/repo-card/badge/badge.tsx
@@ -1,6 +1,6 @@
 export function Badge() {
   return (
-    <div className="absolute -right-2 -top-3 rounded-lg border border-stone-400 bg-indigo-50 px-2 py-1 text-xs font-semibold text-black dark:bg-neutral-900 dark:text-indigo-50">
+    <div className="absolute -top-3 -right-2 rounded-lg border border-stone-400 bg-indigo-50 px-2 py-1 text-xs font-semibold text-black dark:bg-neutral-900 dark:text-indigo-50">
       New!
     </div>
   );
