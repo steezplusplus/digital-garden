f6b1a17406f66a714d0433445f953fc996a5fb3e
Jesse Breuer <jessebreuer@Mac.lan>
2025-09-29 12:20:36 -0400

    feat: Create grid component

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,54 +1,45 @@
-export default function NewPage() {
+import Grid from '@/components/grid';
+
+export default function NewGridPage() {
   return (
     <>
-      {/* This section should only contain static content */}
-      <Section label="Contact Information">
-        <Card styles='sm:sticky sm:top-10 self-start aspect-square'>Featured</Card>
-
-        <Grid styles="grid-cols-1">
-          <Card>Theme</Card>
-
-          <Grid styles="grid-cols-2">
-            <Card styles="aspect-square">GitHub</Card>
-            <Card styles="aspect-square">Twitter</Card>
-            <Card styles="aspect-square">Discord</Card>
-            <Card styles="aspect-square">Bluesky</Card>
+      <section aria-labelledby='Contact Information'>
+        <Grid className='grid-cols-1 sm:grid-cols-2'>
+          <Grid.Item className='sm:sticky sm:top-10 self-start aspect-square'>Featured</Grid.Item>
+          <Grid className='grid-cols-1'>
+            <Grid.Item>Theme</Grid.Item>
+            <Grid className='grid-cols-2'>
+              <Grid.Item className='aspect-square'>1</Grid.Item>
+              <Grid.Item className='aspect-square'>2</Grid.Item>
+              <Grid.Item className='aspect-square'>3</Grid.Item>
+              <Grid.Item className='aspect-square'>4</Grid.Item>
+            </Grid>
+            <Grid.Item>Support Me</Grid.Item>
           </Grid>
-
-          <Card>Support me</Card>
         </Grid>
-      </Section>
+      </section>
 
-      {/* This section should contain dynamic data fetched from my github profile */}
-      <Section label="Projects">
-        <Grid styles="grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-          sm:grid-cols-2 sm:gap-6 md:grid-cols-3 md:gap-8
-          <Card>Project 1</Card>
-          <Card>Project 1</Card>
-          <Card>Project 1</Card>
+      <section aria-labelledby='Projects'>
+        <h2 className="mb-4 text-3xl">
+          Most recently updated projects
+        </h2>
+        <Grid className="grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <Grid.Item className='aspect-square'>1</Grid.Item>
+          <Grid.Item className='aspect-square'>2</Grid.Item>
+          <Grid.Item className='aspect-square'>3</Grid.Item>
         </Grid>
-      </Section>
+      </section>
 
-      {/* This section should only contain dynamic data from various unimportant online profiles */}
-      <Section label="About Me">
-          <Card>Spotify</Card>
-      </Section>
+      <section aria-labelledby='About'>
+        <h2 className="mb-4 text-3xl">
+          Profiles
+        </h2>
+        <Grid className="grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <Grid.Item className='aspect-square'>Leetcode</Grid.Item>
+          <Grid.Item className='aspect-square'>Typeracer</Grid.Item>
+          <Grid.Item className='aspect-square'>Spotify</Grid.Item>
+        </Grid>
+      </section>
     </>
   );
-}
-
-function Section({ label, children }: { label: string, children: React.ReactNode }) {
-  return (
-    <section aria-labelledby={label} className="grid grid-cols-1 sm:grid-cols-2 gap-4">{children}</section>
-  );
-}
-
-function Grid({ styles, children }: { styles?: string, children: React.ReactNode }) {
-  // TODO: Better string concat approach for combining classes
- return <div className={`grid gap-4${styles ? ' ' + styles : ''}`}>{children}</div>
-}
-
-function Card({ styles, children }: { styles?: string, children: React.ReactNode }) {
-  // TODO: Better string concat approach for combining classes
-  return <div className={`border rounded border-gray-200 p-4${styles ? ' ' + styles: ''}`}>{children}</div>
 }
---
+++ src/components/grid/index.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+function Grid(props: React.ComponentProps<'ul'>) {
+  return (
+    <ul {...props} className={clsx('grid gap-4', props.className)}>
+      {props.children}
+    </ul>
+  );
+}
+
+function GridItem(props: React.ComponentProps<'li'>) {
+  return (
+    <li {...props} className={clsx('rounded-xl border border-stone-400 bg-white/50 p-4 dark:bg-indigo-100/5', props.className)}>
+      {props.children}
+    </li>
+  );
+}
+
+Grid.Item = GridItem;
+
+export default Grid;
