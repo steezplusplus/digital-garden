1a07742ab8cf919e7e33afd09097b33ab9d6b464
Jesse Breuer <jessebreuer@Mac.lan>
2025-09-30 18:05:28 -0400

    feat(remove unused components): remove unused ocmponents

--- src/components/cards/profile-card/profile-card.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import { ProfileCard } from './profile-card';
-
-describe('<ProfileCard />', () => {
-  it('Should render with no errors', () => {
-    render(<ProfileCard />);
-  });
-});
--- src/components/cards/repo-card/badge/badge.tsx
+++
@@ -1,7 +0,0 @@
-export function Badge() {
-  return (
-    <div className="absolute -top-3 -right-2 rounded-lg border border-stone-400 bg-indigo-50 px-2 py-1 text-xs font-semibold text-black dark:bg-neutral-900 dark:text-indigo-50">
-      New!
-    </div>
-  );
-}
