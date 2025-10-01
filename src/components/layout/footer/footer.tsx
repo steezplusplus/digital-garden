934f8b958325dfeeded3606283f4fc98bc603dac
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-01 14:28:23 -0400

    feat: fix footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
-const githubRepoUrl = 'https://github.com/steezplusplus/Portfolio';
+const githubRepoUrl = 'https://github.com/steezplusplus/digital-garden';
 
 export function Footer() {
   return (
     <ul className="flex h-full flex-col items-center justify-center px-4 text-sm sm:flex-row sm:justify-between sm:px-6 lg:px-8">
       <li>
-        <p>&copy; Jesse Breuer-Penello. All rights reserverd.</p>
+        <p>&copy; Jesse Breuer-Penello. All rights reserved.</p>
       </li>
       <li>
         <Link
