ce6202f2e9615f191fcad873aaf111831618e670
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-01 13:59:04 -0400

    feat: re add theme card

--- src/components/cards/theme-card/theme-card.tsx
+++ src/components/cards/theme-card/theme-card.tsx
@@ -1,10 +1,14 @@
 import { SunDim } from 'lucide-react';
 
+import { ThemeSelect } from '@/components/theme-select/theme-select';
+
 export default function ThemeCard() {
   return (
     <div className="flex flex-col rounded-xl border border-stone-400 bg-white/50 p-4 dark:bg-indigo-100/5">
       <h2 className="mb-1 self-end text-lg">Theme</h2>
-      <div className="my-auto flex justify-center">x</div>
+      <div className="my-auto flex justify-center">
+        <ThemeSelect />
+      </div>
       <div className="self-start text-sm italic">
         <SunDim />
       </div>
