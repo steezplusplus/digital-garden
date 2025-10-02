2046b8b7fa3e01e3929b1cde42ac0be5745e0006
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-02 12:46:51 -0400

    test: unit test theme

--- src/components/providers/providers.test.tsx
+++ src/components/providers/providers.test.tsx
@@ -1,31 +1,31 @@
 import { render, screen } from '@testing-library/react';
-
 import { Providers } from './providers';
 
 describe('<Providers />', () => {
   beforeEach(() => {
+
     Object.defineProperty(window, 'matchMedia', {
       writable: true,
       value: jest.fn().mockImplementation((query) => ({
         matches: false,
         media: query,
         onchange: null,
-        addListener: jest.fn(), // Deprecated
-        removeListener: jest.fn(), // Deprecated
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
         addEventListener: jest.fn(),
         removeEventListener: jest.fn(),
         dispatchEvent: jest.fn(),
       })),
     });
   });
-  const children = (
-    <>
-      <p data-testid="test">find me</p>
-    </>
-  );
-  it('Should render with no errors', () => {
-    render(<Providers>{children}</Providers>);
-    const para = screen.getByTestId('test');
-    expect(para).toBeInTheDocument();
+
+  it('renders children inside the ThemeProvider', () => {
+    render(
+      <Providers>
+        <div data-testid="child">hello</div>
+      </Providers>
+    );
+
+    expect(screen.getByTestId('child')).toBeInTheDocument();
   });
 });
