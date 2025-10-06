057f291ff082675c2d63b3ef872716abc2fc65fd
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-06 13:49:34 -0400

    feat: show some stats from steam

--- src/api/steam.ts
+++ src/api/steam.ts
@@ -5,23 +5,53 @@ const STEAM_WEB_API_KEY = process.env.STEAM_WEB_API_KEY;
 const STEAM_ID = process.env.STEAM_ID;
 
 const isDev = NODE_ENV === 'development';
-const BASE_URL = 'http://api.steampowered.com/'
+const BASE_URL = 'http://api.steampowered.com'
 
-// http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=XXXXXXXXXXXXXXXXX&steamid=76561197960434622&format=json
-export async function getSteamPlaytime() {
-  const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_WEB_API_KEY}&steamid=${STEAM_ID}&format=json`;
+type SteamOwnedGamesResponse = {
+  response: {
+    game_count: number;
+    games: {
+      appid: string;
+      playtime_forever: number;
+      playtime_windows_forever: number;
+      playtime_mac_forever: number;
+      playtime_linux_forever: number;
+      playtime_deck_forever: number;
+      rtime_last_played: number;
+      playtime_disconnected: number;
+    }[];
+  };
+};
 
-  const res = await fetch(url);
+type SteamStatistics = {
+  totalGames: number;
+  totalPlaytime: number; // in minutes
+}
 
-  if (!res.ok) {
-    console.log(res.statusText);
-  }
-
-  const data = await res.json();
+// https://developer.valvesoftware.com/wiki/Steam_Web_API#GetOwnedGames_(v0001)
+export async function getSteamStatistics(): Promise<SteamStatistics> {
+  const url = `${BASE_URL}/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_WEB_API_KEY}&steamid=${STEAM_ID}&format=json`;
 
   if (isDev) {
-    console.debug('[steam]', data);
+    console.info('[steam] request', { method: 'GET', route: '/IPlayerService/GetOwnedGames' });
   }
 
-  return data;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    console.log(response.statusText);
+  }
+
+  if (isDev) {   
+    console.info('[steam] response', { status: response.status, route: '/IPlayerService/GetOwnedGames' });
+  }
+
+  const data: SteamOwnedGamesResponse = await response.json();
+
+  const stats = {
+    totalGames: data.response.game_count,
+    totalPlaytime: data.response.games.reduce((acc, game) => acc + game.playtime_forever, 0),
+  };
+
+  return stats;
 }
--- src/components/layout/sections/about-section.tsx
+++ src/components/layout/sections/about-section.tsx
@@ -1,10 +1,20 @@
-import { getSteamPlaytime } from '@/api/steam';
+import { Gamepad } from 'lucide-react';
+
+import { getSteamStatistics } from '@/api/steam';
 
 export default async function AboutSection() {
-  const data = await getSteamPlaytime();
-  return <pre>{JSON.stringify(data, null, 4)}</pre>;
+  const data = await getSteamStatistics();
+
+  return (
+    <div className='rounded-xl border border-stone-400 bg-white/50 p-4 dark:bg-indigo-100/5'>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+      <p>Owns {data.totalGames} on steam with {Math.floor(data.totalPlaytime / 60)} hours played.</p>
+      <Gamepad />
+    </div>
+  );
 }
 
 export function AboutSectionSkeleton() {
   return <div>Loading Steam data...</div>;
 }
+
