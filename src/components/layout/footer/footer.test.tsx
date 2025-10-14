63d65f06b52d76dbff6bae88ed2d4767bbde1190
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-14 14:37:09 -0400

    feat: update footer and tests

--- src/components/layout/footer/footer.test.tsx
+++ src/components/layout/footer/footer.test.tsx
@@ -1,10 +1,30 @@
 import { render, screen } from '@testing-library/react';
-import { Footer } from './footer';
+import { Footer, githubRepoUrl, unlicenseUrl } from './footer';
 
 describe('<Footer />', () => {
-  it('Should render an anchor', () => {
+  it('should render both links', () => {
     render(<Footer />);
-    const anchor = screen.getByRole('link');
-    expect(anchor).toBeInTheDocument();
+    const links = screen.getAllByRole('link');
+    expect(links).toHaveLength(2);
+  });
+
+  it('should render Unlicense link with correct attributes', () => {
+    render(<Footer />);
+    const unlicenseLink = screen.getByRole('link', { name: /unlicense/i });
+    
+    expect(unlicenseLink).toBeInTheDocument();
+    expect(unlicenseLink).toHaveAttribute('href', unlicenseUrl);
+    expect(unlicenseLink).toHaveAttribute('target', '_blank');
+    expect(unlicenseLink).toHaveAttribute('rel', 'noopener noreferrer');
+  });
+
+  it('should render Source link with correct attributes', () => {
+    render(<Footer />);
+    const sourceLink = screen.getByRole('link', { name: /source/i });
+    
+    expect(sourceLink).toBeInTheDocument();
+    expect(sourceLink).toHaveAttribute('href', githubRepoUrl);
+    expect(sourceLink).toHaveAttribute('target', '_blank');
+    expect(sourceLink).toHaveAttribute('rel', 'noopener noreferrer');
   });
 });
