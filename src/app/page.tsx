59e41e9916ccc7cb6322fd9a2cddaa7b07bacc6f
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-14 13:51:06 -0400

    feat: fix revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Suspense } from 'react';
 import Gallery from '@/components/layout/sections/gallery';
 import Projects, { ProjectsSkeleton } from '@/components/layout/sections/projects';
 
+export const revalidate = 3600; // Revalidate this page every hour.
+
 export const metadata: Metadata = {
   title: "Jesse's Digital Garden",
 };
