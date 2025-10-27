ae581e3ec0599636d7e376226898fe5d6044675e
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-27 14:58:36 -0400

    feat: pretty

--- src/components/cards/jukebox-card/jukebox.test.tsx
+++ src/components/cards/jukebox-card/jukebox.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import { Jukebox } from './jukebox';
 
-// Someday I will test this. But not today. 
+// Someday I will test this. But not today.
 describe('<Jukebox />', () => {
   it('renders the component', () => {
     render(<Jukebox />);
