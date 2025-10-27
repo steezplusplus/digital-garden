7553c8fe12ce3a123f6a0ca45bf6e8a56faf5375
Jesse Breuer <jessebreuer@Mac.lan>
2025-10-27 14:57:02 -0400

    refactor: add note

--- src/components/cards/jukebox-card/jukebox.test.tsx
+++ src/components/cards/jukebox-card/jukebox.test.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 
 import { Jukebox } from './jukebox';
 
+// Someday I will test this. But not today. 
 describe('<Jukebox />', () => {
   it('renders the component', () => {
     render(<Jukebox />);
