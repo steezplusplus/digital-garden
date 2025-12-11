a717fc723a060ed9632d75431bb1c6dcad8294f1
Jesse B. <9201173+steezplusplus@users.noreply.github.com>
2025-12-11 14:29:43 -0500

    build: use nextjs codemode to upgrade to 16 (#328)
    * build: use nextjs codemode to upgrade to 16
    
    * build: run eslint codemod
    
    * fix: eslint ignores coverage
    
    * refactor: run prettier
    
    * fix: fix linter error
    
    * feat: remove old unused eslint config
    
    ---------
    
    Co-authored-by: Jesse B <USERNAME@users.noreply.github.com>

--- src/components/theme-select/theme-select.tsx
+++ src/components/theme-select/theme-select.tsx
@@ -1,41 +1,21 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 
 export function ThemeSelect() {
-  const [mounted, setMounted] = useState<boolean>(false);
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => {
-    setMounted(true);
-  }, [theme]);
-
-  if (!mounted) {
-    return (
-      <div className="my-auto flex justify-center">
-        <select className="rounded-md border border-slate-400 p-2 text-xs">
-          <option value="system">System</option>
-          <option value="dark">Dark</option>
-          <option value="light">Light</option>
-        </select>
-      </div>
-    );
-  }
-
   return (
-    <>
-      <div className="my-auto flex justify-center">
-        <select
-          className="rounded-md border border-slate-400 p-2 text-xs"
-          value={theme}
-          onChange={(e) => setTheme(e.target.value)}
-        >
-          <option value="system">System</option>
-          <option value="dark">Dark</option>
-          <option value="light">Light</option>
-        </select>
-      </div>
-    </>
+    <div className="my-auto flex justify-center">
+      <select
+        className="rounded-md border border-slate-400 p-2 text-xs"
+        value={theme}
+        onChange={(e) => setTheme(e.target.value)}
+      >
+        <option value="system">System</option>
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+      </select>
+    </div>
   );
 }
