25baa183a135c5b04c8172cd7e1195b04f6d1e47
Jesse B <USERNAME@users.noreply.github.com>
2025-12-11 13:57:13 -0500

    refactor: run prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,28 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
+export default defineConfig([
+  {
     extends: [
-        ...nextCoreWebVitals,
-        ...compat.extends("plugin:testing-library/react"),
-        ...compat.extends("plugin:jest-dom/recommended"),
-        ...compat.extends("prettier")
-    ],   
-}, {
-    ignores: ["coverage/**"],
-    }]);
+      ...nextCoreWebVitals,
+      ...compat.extends('plugin:testing-library/react'),
+      ...compat.extends('plugin:jest-dom/recommended'),
+      ...compat.extends('prettier'),
+    ],
+  },
+  {
+    ignores: ['coverage/**'],
+  },
+]);
