ed0f3292cb41524ad08b7e159b45e389f8750d41
Jesse <9201173+steezplusplus@users.noreply.github.com>
2026-09-17 14:45:34 -0400

    refactor: run prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Lora } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import './globals.css';
 import { Nav } from '@/components/layout/nav/nav';
