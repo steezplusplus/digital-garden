d3957338f50c02c49e545aee6ee8b0df03251621
Jesse B. <9201173+steezplusplus@users.noreply.github.com>
2026-09-17 15:00:14 -0400

    feat: install and configure vercel speed insights (#348)
    * feat: install and configure vercel speed insights
    
    * refactor: run prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Lora } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import './globals.css';
 import { Nav } from '@/components/layout/nav/nav';
@@ -40,7 +41,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={lora.className}>
-        <Analytics />
         <Providers>
           <nav>
             <Nav />
@@ -50,6 +50,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Footer />
           </footer>
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
